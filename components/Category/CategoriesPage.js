c8eefd56a63c77cfb95de17055d3c3011a2c301c
Happy Tom <mithunaksh01@gmail.com>
2024-09-30 23:58:36 +0530

    Category delete

--- components/Category/CategoriesPage.js
+++ components/Category/CategoriesPage.js
@@ -3,16 +3,29 @@ import { ScrollView } from "react-native";
 import { CategoriesExpensesContext } from "../Context/CategoriesExpensesContext";
 import AddCategoryForm from "./AddCategoryForm";
 import CategoryList from "./CategoryList";
+import axios from "axios";
 
 const CategoriesPage = () => {
   const { categories, fetchCategoriesAndExpenses } = useContext(
     CategoriesExpensesContext
   );
 
+  const handleDeleteCategory = async (id) => {
+    try {
+      await axios.delete(`http://192.168.1.43:8000/api/categories/${id}/`);
+      fetchCategoriesAndExpenses();
+    } catch (error) {
+      console.error("Error deleting category:", error);
+    }
+  };
+
   return (
     <ScrollView>
       <AddCategoryForm fetchCategories={fetchCategoriesAndExpenses} />
-      <CategoryList categories={categories} />
+      <CategoryList
+        categories={categories}
+        handleDelete={handleDeleteCategory} // Pass the delete handler to CategoryList
+      />
     </ScrollView>
   );
 };
--- components/Category/CategoryList.js
+++ components/Category/CategoryList.js
@@ -29,11 +29,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
 });
 
 export default CategoryList;
